33993387b68b32b37db30ed8d3140fcebad604c1
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-05-29 18:06:20 -0400

    Navbar añadido

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,19 @@
 <script>
-	export let name;
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import "bootstrap/dist/js/bootstrap.bundle"
+
+	import { Router, Route } from "svelte-navigator";
+
+	// Components
+	import Navbar from "./components/Navbar.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router>
+	<main>
+		<Navbar/>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+		<Route path="/">
+			
+		</Route>
+	</main>
+</Router>
