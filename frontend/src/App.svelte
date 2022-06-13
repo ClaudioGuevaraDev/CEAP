4c40b3a5448d57838fc9054090fd6aa02f2e6471
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-06-13 17:16:37 -0400

    Navbar agregado

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,8 +6,13 @@
 
   // Screens
   import Home from "./screens/Home.svelte";
+
+  //Components
+  import Navbar from "./components/Navbar.svelte";
 </script>
 
 <Router>
+  <Navbar/>
+
   <Route path="/" component={Home} />
 </Router>
---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { Link } from "svelte-navigator"
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light p-2">
+  <div class="container-fluid">
+    <span class="h2">Centro de Estudios en Alimentos Procesados</span>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <Link to="/" class="nav-link">
+            <button class="btn btn-danger btn-lg">Cerrar Sesión</button>
+        </Link>
+      </div>
+    </div>
+  </div>
+</nav>
+
