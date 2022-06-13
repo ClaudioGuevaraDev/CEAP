820fe0bd5efd6cd60f408644309ec6bef7793ccf
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-06-13 17:06:10 -0400

    proyecto de svelte creado, enrutador agregado y bootstrap añadido

---
+++ frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import "bootstrap/dist/js/bootstrap.bundle";
+
+  import { Router, Route } from "svelte-navigator";
+
+  // Screens
+  import Home from "./screens/Home.svelte";
+</script>
+
+<Router>
+  <Route path="/" component={Home} />
+</Router>
